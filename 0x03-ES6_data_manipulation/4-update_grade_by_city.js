f5fe84ab3fecf67a5691b4052d99dc5b57593bce
TayArmy <alabenitemitayo@gmail.com>
2023-07-04 04:06:46 +0000

    manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,16 @@
-export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr
-    .filter(student => student.location === city)
-    .map(student => {
-      let grade = newGrades.find(g => g.studentId === student.id);
-      return { ...student, grade: (grade && grade.grade) || 'N/A' };
+import getListStudents from "./0-get_list_students.js";
+
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+      if (matchingGrade) {
+        return { ...student, grade: matchingGrade.grade };
+      } else {
+        return { ...student, grade: "N/A" };
+      }
     });
 }
+
+export default updateStudentGradeByCity;
